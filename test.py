e5b63359f20eee01abfce3872a5b3070e14eaa17
JW0203 <widwoo0203@gmail.com>
2023-11-30 20:49:20 +0900

    fit: check odd number

--- test.py
+++ test.py
@@ -3,4 +3,11 @@ print('Hello word!!')
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(nums[::2])
 
-print('hello ' *3)
+print('hello' * 3)
+
+# check odd number
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
